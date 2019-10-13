6f9cc1ea33d4beb44f24bdcc288b32f307504a1c
Imagic <imagicthecat@gmail.com>
2019-10-14 00:14:46 +0200

    Improve host description.

--- hosts/x86-64_i5-6500.lua
+++ hosts/x86-64_i5-6500.lua
@@ -1,9 +1,9 @@
 return {
   title = "x86_64 i5-6500 3.6GHz 8Go DDR4",
   description = [[
-4.9.11-1-ARCH x86_64 GNU/Linux (Archlinux)
-i5-6500 3.6Gz
-8Go DDR4
+OS: 4.9.11-1-ARCH x86_64 GNU/Linux (Archlinux)
+CPU: i5-6500 3.6Gz
+RAM: 8Go DDR4
   ]],
   measures = 3,
   timeout = 60, -- process timeout, seconds
