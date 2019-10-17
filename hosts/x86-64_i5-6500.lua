32c3a4a222732d00d964cedc351f589efc37e2a9
Imagic <imagicthecat@gmail.com>
2019-10-17 23:28:48 +0200

    Add more host info.

--- hosts/x86-64_i5-6500.lua
+++ hosts/x86-64_i5-6500.lua
@@ -1,8 +1,8 @@
 return {
   title = "x86_64 i5-6500 3.6GHz 8Go DDR4",
   description = [[
-OS: 4.9.11-1-ARCH x86_64 GNU/Linux (Archlinux)
-CPU: i5-6500 3.6Gz
+OS: (4.9.11-1-ARCH 5.3.6-arch1-1-ARCH) x86_64 GNU/Linux (Archlinux)
+CPU: i5-6500 3.6Gz 4 cores
 RAM: 8Go DDR4
 
 Maintained by ImagicTheCat.
