e5ff5cc8e9f6686df57a559db8f0079a35cd5215
Imagic <imagicthecat@gmail.com>
2019-10-18 20:41:34 +0200

    Add some C implementation vars.

---
+++ langs/cpp/envs/gcc-any.lua
@@ -0,0 +1,22 @@
+local path = os.getenv("GCC_PP_PATH") or "g++"
+
+local f = io.popen(path.." --version")
+local version = f:read("*a")
+f:close()
+
+return {
+  version = 1,
+  title = "GCC",
+  description = [[
+https://gcc.gnu.org/
+
+IMPL VARS:
+- LIBS: GCC lib link flags
+- OPTS: GCC options
+  ]],
+  host_info = version,
+  build = function(e)
+    return os.execute(path.." "..e.p_impl.." -o "..e.p_tmp.."/run "..(e.vars.LIBS or "").." "..(e.vars.OPTS or "")) == 0
+  end,
+  run_cmd = function(e, ...) return e.p_tmp.."/run", ... end
+}
