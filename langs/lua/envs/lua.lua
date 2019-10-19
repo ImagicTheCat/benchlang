10138f8428fc459cdf3f4000069f4cf318bb9afb
Imagic <imagicthecat@gmail.com>
2019-10-19 18:24:13 +0200

    Fix lua/lua env issue.

--- langs/lua/envs/lua.lua
+++ langs/lua/envs/lua.lua
@@ -13,6 +13,6 @@ https://www.lua.org/
 Official VM implementation (latest).
   ]],
   host_info = version,
-  build = function(impl_path, tmp_path) return true end,
-  run_cmd = function(impl_path, tmp_path, ...) return path, impl_path, ... end
+  build = function(e) return true end,
+  run_cmd = function(e, ...) return path, e.p_impl, ... end
 }
